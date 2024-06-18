410806d9b768edadd7971882eb651b0ecfc4a270
FraCuboni <fracuboni@gmail.com>
2024-06-18 15:39:05 +0200

    feata created basic array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
 // MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// Stampare su console, per ogni membro del team, le informazioni di"nome" ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 // creo l'array di oggetti con le informazioni fornite
+const teamMembers[
+    {
+        "nome": "Wayne";
+        "cognome": "barnett";
+        "ruolo": "Founder & CEO";
+        "img": "wayne-barnett-founder-ceo.jpg";
+    },
+    {
+        "nome": "Wayne";
+        "cognome": "barnett";
+        "ruolo": "Founder & CEO";
+        "img": "wayne-barnett-founder-ceo.jpg";
+    },
+    {
+        "nome": "Wayne";
+        "cognome": "barnett";
+        "ruolo": "Founder & CEO";
+        "img": "wayne-barnett-founder-ceo.jpg";
+    },
+    {
+        "nome": "Wayne";
+        "cognome": "barnett";
+        "ruolo": "Founder & CEO";
+        "img": "wayne-barnett-founder-ceo.jpg";
+    },
+    {
+        "nome": "Wayne";
+        "cognome": "barnett";
+        "ruolo": "Founder & CEO";
+        "img": "wayne-barnett-founder-ceo.jpg";
+    },
+    {
+        "nome": "Wayne";
+        "cognome": "barnett";
+        "ruolo": "Founder & CEO";
+        "img": "wayne-barnett-founder-ceo.jpg";
+    },
+]
