5a1a98531a72bf3bc89d4883acbc01ece367be33
FraCuboni <fracuboni@gmail.com>
2024-06-18 15:42:28 +0200

    feat main array working

--- js/script.js
+++ js/script.js
@@ -1,46 +1,49 @@
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
+
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di"nome" ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 // creo l'array di oggetti con le informazioni fornite
-const teamMembers[
+const teamMembers=[
     {
-        "nome": "Wayne";
-        "cognome": "barnett";
-        "ruolo": "Founder & CEO";
-        "img": "wayne-barnett-founder-ceo.jpg";
+        "nome": "Wayne",
+        "cognome": "barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
     {
-        "nome": "Wayne";
-        "cognome": "barnett";
-        "ruolo": "Founder & CEO";
-        "img": "wayne-barnett-founder-ceo.jpg";
+        "nome": "MARIO",
+        "cognome": "barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
     {
-        "nome": "Wayne";
-        "cognome": "barnett";
-        "ruolo": "Founder & CEO";
-        "img": "wayne-barnett-founder-ceo.jpg";
+        "nome": "GIROGIO",
+        "cognome": "barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
     {
-        "nome": "Wayne";
-        "cognome": "barnett";
-        "ruolo": "Founder & CEO";
-        "img": "wayne-barnett-founder-ceo.jpg";
+        "nome": "ELENe",
+        "cognome": "barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
     {
-        "nome": "Wayne";
-        "cognome": "barnett";
-        "ruolo": "Founder & CEO";
-        "img": "wayne-barnett-founder-ceo.jpg";
+        "nome": "GAIA",
+        "cognome": "barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
     {
-        "nome": "Wayne";
-        "cognome": "barnett";
-        "ruolo": "Founder & CEO";
-        "img": "wayne-barnett-founder-ceo.jpg";
+        "nome": "LUCIA",
+        "cognome": "barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
 ]
+
+console.log(teamMembers[2]);
